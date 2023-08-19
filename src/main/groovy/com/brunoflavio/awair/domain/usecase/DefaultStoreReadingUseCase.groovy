a9b8562bad84e0a51cfc4d19876337c3138783a6
Bruno Flávio <email@brunoflavio.com>
2023-08-19 14:14:11 +0100

    Wires up the repository interface to the store readings usecase.

--- src/main/groovy/com/brunoflavio/awair/domain/usecase/DefaultStoreReadingUseCase.groovy
+++ src/main/groovy/com/brunoflavio/awair/domain/usecase/DefaultStoreReadingUseCase.groovy
@@ -1,16 +1,24 @@
 package com.brunoflavio.awair.domain.usecase
 
+
 import com.brunoflavio.awair.domain.model.AwairReading
+import com.brunoflavio.awair.infrastructure.repository.ReadingsRepository
 import groovy.transform.CompileStatic
 import jakarta.inject.Singleton
 import org.reactivestreams.Publisher
-import reactor.core.publisher.Mono
 
 @Singleton
 @CompileStatic
 class DefaultStoreReadingUseCase implements StoreReadingUseCase {
+
+    private final ReadingsRepository readingsRepository
+
+    DefaultStoreReadingUseCase(ReadingsRepository readingsRepository) {
+        this.readingsRepository = readingsRepository
+    }
+
     @Override
     Publisher<AwairReading> persist(AwairReading reading) {
-        Mono.just(reading)
+        readingsRepository.save(reading)
     }
 }
--- src/test/groovy/com/brunoflavio/awair/domain/usecase/DefaultStoreReadingUseCaseSpec.groovy
+++ src/test/groovy/com/brunoflavio/awair/domain/usecase/DefaultStoreReadingUseCaseSpec.groovy
@@ -1,15 +1,23 @@
 package com.brunoflavio.awair.domain.usecase
 
 import com.brunoflavio.awair.domain.model.AwairReading
+import com.brunoflavio.awair.infrastructure.repository.ReadingsRepository
+import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
 import spock.lang.Specification
 
 class DefaultStoreReadingUseCaseSpec extends Specification {
 
+
     def "persist should return the provided reading"() {
-        given: "a reading and the DefaultStoreReadingUseCase"
-        def reading = new AwairReading()
-        def useCase = new DefaultStoreReadingUseCase()
+        given: "a reading"
+        AwairReading reading = new AwairReading()
+
+        and: "a mock repository"
+        ReadingsRepository readingsRepositoryMock = Mock(ReadingsRepository)
+
+        and: "a DefaultStoreReadingUseCase is created"
+        StoreReadingUseCase useCase = new DefaultStoreReadingUseCase(readingsRepositoryMock)
 
         when: "calling the persist method"
         def result = useCase.persist(reading)
@@ -19,5 +27,8 @@ class DefaultStoreReadingUseCaseSpec extends Specification {
                 .expectNext(reading)
                 .expectComplete()
                 .verify()
+
+        and: "the mock repository was called once"
+        1 * readingsRepositoryMock.save(reading) >> Mono.just(reading)
     }
 }
